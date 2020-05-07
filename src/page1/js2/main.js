2cb937c3f4ffabb9347d04b8baf242fdc7d49d0c
Fred Garcia <Fred@Garcia.com>
2020-05-06 18:08:15 -0700

    intial commit

--- src/page1/js2/main.js
+++ src/page1/js2/main.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import img from '../img/space-research-science-astronaut-41162.jpg';
+import img from '../img/silhouette-of-person-holding-glass-mason-jar-1274260.jpg';
 import css from '../style/pod.css';
 
 
