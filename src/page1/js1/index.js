4c95e90e7e5c481871fbde9cf8e45ab2502d03fd
Fred Garcia <Fred@Garcia.com>
2020-05-06 18:36:54 -0700

    img

--- src/page1/js1/index.js
+++ src/page1/js1/index.js
@@ -1,2 +1,3 @@
 import css from '../style/style.css'
-import img from '../img/silhouette-of-person-holding-glass-mason-jar-1274260.jpg';
+// import image from '..\img\silhouette-of-person-holding-glass-mason-jar-1274260.jpg';
+import img from '../img/gray-and-white-robot-73910 (1).jpg';
