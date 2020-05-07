6f8364b5778629399aae39d6e07eda3975b44067
Fred Garcia <Fred@Garcia.com>
2020-05-06 19:03:37 -0700

    initial commit

--- src/page1/js1/index.js
+++ src/page1/js1/index.js
@@ -1,3 +1,2 @@
 import css from '../style/style.css'
-// import image from '..\img\silhouette-of-person-holding-glass-mason-jar-1274260.jpg';
-import img from '../img/gray-and-white-robot-73910 (1).jpg';
+import img from '../img/robot.jpg';
