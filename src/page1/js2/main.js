db7d34023a3c84094ab41dbef786fe00162b2dad
Fred Garcia <Fred@Garcia.com>
2020-06-29 18:45:09 -0700

    removed click hanlder

--- src/page1/js2/main.js
+++ src/page1/js2/main.js
@@ -8,109 +8,101 @@ import * as spinner from '../../page2/spinner';
 
 //Get request
 
+//event listener
+// document.getElementById('pod').addEventListener('click', getPicture);
+// const pod = document.querySelector('pod');
 
-const getPicture = () => {
+
    spinner.spinner();
     axios({
         method:'get',
         url:'https://api.nasa.gov/planetary/apod?api_key=6YmFdsVxaR5ewRLqHtIEIFBPM1jsqFUxjGAJPBPV&hd=false'
     })
-    .then(res => showOutput(res))
+    .then(res => {
+        if(res.data.media_type == "video" && res.data.copyright) {  
+            spinner.clearSpinner();
+            document.getElementById('p').innerHTML = `
+            <h2> ${res.data.title} </h2>
+            <iframe class="video"
+            src= "${res.data.url}">
+            </iframe>
+            `;
+            document.getElementById('p2').innerHTML= `
+            <h2> About this picture </h2>
+            <p>${res.data.explanation}</p>
+            `;
+            document.querySelector('.info-display').innerHTML = `
+            <p>Uploaded: ${res.data.date}</p>
+            <p class="credit">Credit: ${res.data.copyright}</p>
+            `;
+            document.getElementById('credit').innerHTML= `
+            <p>&copy Design by Fred Garcia 2020</p>
+            `;
+        }
+        else if ( res.data.media_type == "video" && !res.data.copyright ) {
+            spinner.clearSpinner();
+            document.getElementById('p').innerHTML = `
+            <h2> ${res.data.title} </h2>
+            <iframe class="video"
+            src= "${res.data.url}">
+            </iframe>
+            `;
+            document.querySelector('.info-display').innerHTML = `
+            <p>Uploaded: ${res.data.date}</p>
+            <p>Credit: NASA</p>
+            `;
+            document.getElementById('p2').innerHTML= `
+            <h2> About this picture </h2>
+            <p>${res.data.explanation}</p>
+            `;
+            document.getElementById('credit').innerHTML= `
+            <p>&copy Design by Fred Garcia 2020</p>
+            `;
+        }
+        if (res.data.media_type == "image" && res.data.copyright) {
+            spinner.clearSpinner();
+            document.getElementById('p').innerHTML= `
+            <h2>${res.data.title}</h2>
+            <a href = "${res.data.url}" target = "_blank"> <img src="${res.data.url}"> </a>
+            `;
+            document.getElementById('p2').innerHTML= `
+            <h2> About this picture </h2>
+            <p> ${res.data.explanation} </p>
+            `;
+            document.querySelector('.info-display').innerHTML = `
+            <p>Uploaded: ${res.data.date}</p>
+            <p class="credit">Credit: ${res.data.copyright}</p>
+            `;
+           
+            document.getElementById('credit').innerHTML= `
+            <p>&copy Design by Fred Garcia 2020</p>
+            `;
+        }
+        else {
+            spinner.clearSpinner();
+            document.getElementById('p').innerHTML= `
+            <h2>${res.data.title}</h2>
+            <a href = "${res.data.url}" target = "_blank"> <img src="${res.data.url}"> </a>
+            `;
+            document.getElementById('p2').innerHTML= `
+            <h2> About this picture </h2>
+            <p> ${res.data.explanation} </p>
+            `;
+            document.querySelector('.info-display').innerHTML = `
+            <p>Uploaded: ${res.data.date}</p>
+            <p class="credit">Credit: NASA</p>
+            `;
+           
+            document.getElementById('credit').innerHTML= `
+            <p>&copy Design by Fred Garcia 2020</p>
+            `;
+        }
+
+    })
     .catch(error => console.log(error))
-}
 
 
-const showOutput = res => {
-    console.log(res);
    
-    if(res.data.media_type == "video" && res.data.copyright) {
-        let btn = document.getElementById('pod');
-        btn.remove();
-        spinner.clearSpinner();
-        document.getElementById('p').innerHTML = `
-        <h2> ${res.data.title} </h2>
-        <iframe class="video"
-        src= "${res.data.url}">
-        </iframe>
-        `;
-        document.getElementById('p2').innerHTML= `
-        <h2> About this picture </h2>
-        <p>${res.data.explanation}</p>
-        `;
-        document.querySelector('.info-display').innerHTML = `
-        <p>Uploaded: ${res.data.date}</p>
-        <p class="credit">Credit: ${res.data.copyright}</p>
-        `;
-        document.getElementById('credit').innerHTML= `
-        <p>&copy Design by Fred Garcia 2020</p>
-        `;
-    }
-    else if ( res.data.media_type == "video" && !res.data.copyright ) {
-        let btn = document.getElementById('pod');
-        btn.remove();
-        spinner.clearSpinner();
-        document.getElementById('p').innerHTML = `
-        <h2> ${res.data.title} </h2>
-        <iframe class="video"
-        src= "${res.data.url}">
-        </iframe>
-        `;
-        document.querySelector('.info-display').innerHTML = `
-        <p>Uploaded: ${res.data.date}</p>
-        <p>Credit: NASA</p>
-        `;
-        document.getElementById('p2').innerHTML= `
-        <h2> About this picture </h2>
-        <p>${res.data.explanation}</p>
-        `;
-        document.getElementById('credit').innerHTML= `
-        <p>&copy Design by Fred Garcia 2020</p>
-        `;
-    }
-    if (res.data.media_type == "image" && res.data.copyright) {
-        let btn = document.getElementById('pod');
-        btn.remove();
-        spinner.clearSpinner();
-        document.getElementById('p').innerHTML= `
-        <h2>${res.data.title}</h2>
-        <a href = "${res.data.url}" target = "_blank"> <img src="${res.data.url}"> </a>
-        `;
-        document.getElementById('p2').innerHTML= `
-        <h2> About this picture </h2>
-        <p> ${res.data.explanation} </p>
-        `;
-        document.querySelector('.info-display').innerHTML = `
-        <p>Uploaded: ${res.data.date}</p>
-        <p class="credit">Credit: ${res.data.copyright}</p>
-        `;
-       
-        document.getElementById('credit').innerHTML= `
-        <p>&copy Design by Fred Garcia 2020</p>
-        `;
-    }
-    else {
-        let btn = document.getElementById('pod');
-        btn.remove();
-        spinner.clearSpinner();
-        document.getElementById('p').innerHTML= `
-        <h2>${res.data.title}</h2>
-        <a href = "${res.data.url}" target = "_blank"> <img src="${res.data.url}"> </a>
-        `;
-        document.getElementById('p2').innerHTML= `
-        <h2> About this picture </h2>
-        <p> ${res.data.explanation} </p>
-        `;
-        document.querySelector('.info-display').innerHTML = `
-        <p>Uploaded: ${res.data.date}</p>
-        <p class="credit">Credit: NASA</p>
-        `;
-       
-        document.getElementById('credit').innerHTML= `
-        <p>&copy Design by Fred Garcia 2020</p>
-        `;
-    }
                                                                                                                                        
-};
 
-//event listener
-document.getElementById('pod').addEventListener('click', getPicture);
+
